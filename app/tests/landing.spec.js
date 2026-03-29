d0746301d943c29a598083e0ac70d6490ca25869
Matt Zhen <mattzhen@u.boisestate.edu>
2026-03-29 16:59:45 -0600

    Update landing page tests to include image, welcome message, and buttons.

--- app/tests/landing.spec.js
+++ app/tests/landing.spec.js
@@ -7,8 +7,20 @@ test.describe('Landing Page', () => {
   test('should display landing page', async ({ page }) => {
     await page.goto('/');
     await expect(page).toHaveTitle(/My Grocery List/);
+    await expect(page.getByAltText("Stock image of grocery bags")).toBeVisible();
+    await expect(page.getByText(/Welcome to My Grocery List/)).toBeVisible();
+    await expect(page.getByText(/To get started/)).toBeVisible();
+
   });
 
+  test('should correctly navigate pages', async ({ page }) => {
+    await page.goto('/');
+    await page.getByRole('link', {name: /create account/i}).click();
+    await expect(page).toHaveTitle(/Create Account/);
 
+    await page.goto('/');
+    await page.getByRole('link', {name: /login/i}).click();
+    await expect(page).toHaveTitle(/Log in/);
+  });
 
 });
